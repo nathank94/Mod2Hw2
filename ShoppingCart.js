e76015916610dba9f5f768ba40b1fee4481fc08d
Nathan K <94547598+nathank94@users.noreply.github.com>
2022-01-10 15:49:10 -0600

    Update ShoppingCart.js

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,7 +1,26 @@
 class ShoppingCart extends React.Component {
     render() {
         return (
-            <li>{this.props.product.item} {this.props.product.quantity}</li>
-        )
+                <aside className="block col-1">
+      <h2>Cart Items</h2>
+      <div>
+        {cartItems.length === 0 && <div>Cart is empty</div>}
+        {cartItems.map((item) => (
+          <div key={item.id} className="row">
+            <div className="col-2">{item.name}</div>
+            <div className="col-2">
+              <button onClick={() => onRemove(item)} className="remove">
+                -
+              </button>{' '}
+              <button onClick={() => onAdd(item)} className="add">
+                +
+              </button>
+            </div>
+
+            <div className="col-2 text-right">
+              {item.qty} x ${item.price.toFixed(2)}
+            </div>
+          </div>
+        ))}
     }
 }
